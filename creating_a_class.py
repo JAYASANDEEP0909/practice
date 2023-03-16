65f24cbf7bab0e4635d38f82c9b7e7bbc25ae90e
JAYASANDEEP0909 <127980288+JAYASANDEEP0909@users.noreply.github.com>
2023-03-16 12:07:46 +0530

    first commit

---
+++ creating_a_class.py
@@ -0,0 +1,19 @@
+class Robot:
+    def running(self):
+        print(" the robot is runing")
+
+
+robot = Robot()
+
+robot.running()
+
+
+# 2
+class Car:
+    def moving(self):
+        print(" The car is moving at medium speed ")
+
+
+car = Car()
+
+car.moving()
